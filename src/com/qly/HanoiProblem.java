cfdade08ae19f0ff3b19a4105ec1c78812ad620b
lvqiliya <lvqiliya@gmail.com>
2019-06-15 01:03:01 +0800

    [add] The original Hanoi problem

---
+++ src/com/qly/HanoiProblem.java
@@ -0,0 +1,25 @@
+package com.qly;
+
+public class HanoiProblem {
+
+    public static void main(String[] args) {
+        String left = "left";
+        String mid = "mid";
+        String right = "right";
+        move(4, left, mid, right);
+        System.out.println(steps);
+    }
+
+    private static int steps;
+
+    public static void move(int n, String left, String mid, String right) {
+        steps++;
+        if (n == 1) {
+            System.out.println("Move 1 from " + left + " to " + right);
+        } else {
+            move(n - 1, left, right, mid);
+            System.out.println("Move " + n + " from " + left + " to " + right);
+            move(n - 1, mid, left, right);
+        }
+    }
+}
