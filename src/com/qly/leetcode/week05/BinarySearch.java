3127d94d452f2f7e4a8ccc4b2a9dcd6bd089f7e2
qlylv <lvqiliya@gmail.com>
2021-12-14 14:02:48 +0800

    [week05]二分查找

---
+++ src/com/qly/leetcode/week05/BinarySearch.java
@@ -0,0 +1,26 @@
+package com.qly.leetcode.week05;
+
+/**
+ * 704. 二分查找
+ *
+ * @author qlylv
+ */
+public class BinarySearch {
+
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+}
