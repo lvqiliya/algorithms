93b3bf931ab722542e9f837f9bea118d4df0f8c4
qlylv <lvqiliya@gmail.com>
2022-01-08 15:02:09 +0800

    [week06]最长公共子序列

---
+++ src/com/qly/leetcode/week06/LongestCommonSubsequence.java
@@ -0,0 +1,28 @@
+package com.qly.leetcode.week06;
+
+/**
+ * 1143. 最长公共子序列
+ *
+ * @author qlylv
+ */
+public class LongestCommonSubsequence {
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        text1 = " " + text1;
+        text2 = " " + text2;
+        int[][] f = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    f[i][j] = f[i - 1][j - 1] + 1;
+                } else {
+                    f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
+                }
+            }
+        }
+        return f[m][n];
+    }
+
+}
